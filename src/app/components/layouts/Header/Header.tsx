37dea4490af82393fa461d96a8488ec62ce9644c
k_95.21 <kohta0421kochi@gmail.com>
2025-04-15 17:31:21 +0900

    add new subpagetemplate

--- src/app/components/layouts/Header/Header.tsx
+++ src/app/components/layouts/Header/Header.tsx
@@ -26,8 +26,10 @@ const Header: React.FC = () => {
     <header className="fixed top-4 left-0 right-0 flex justify-center items-center w-full z-10">
       <div className="w-11/12 h-[60px] bg-white/20 backdrop-blur-md rounded-lg flex items-center justify-between px-4 md:px-6">
         {/* logo */}
-        <div className="font-semibold text-xl">
-          <p className="text-white">QueryLift</p>
+        <div className="font-semibold text-xl cursor-pointer">
+          <Link href="/" className="text-white cursor-pointer">
+            QueryLift
+          </Link>
         </div>
 
         {/* デスクトップナビゲーション - lg以上で表示 */}
